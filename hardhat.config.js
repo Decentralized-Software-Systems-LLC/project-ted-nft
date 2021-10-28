d912f9a0717ca57db6629ce563af68362395fd20
bighappyface <anomalous20@gmail.com>
2021-10-27 21:36:22 -0400

    Update to Solidity 0.8.9 and add gas reporting

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,12 +3,13 @@ require("hardhat-gas-reporter");
 
 const alchemyRinkebyEndpoint = process.env.ALCHEMY_ENDPOINT_RINKEBY || "https://eth-rinkeby.alchemyapi.io/v2/67890";
 const privateKey = process.env.PRIVATE_KEY || "0000000000000000000000000000000000000000";
+const cmcAPIKey = process.env.CMC_API_KEY || "";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.7",
+  solidity: "0.8.9",
   networks: {
     hardhat: {
       chainId: 1337
@@ -22,6 +23,8 @@ module.exports = {
     }
   },
   gasReporter: {
-    currency: 'USD'
+    currency: "USD",
+    showTimeSpent: true,
+    coinmarketcap: cmcAPIKey
   }
 };
