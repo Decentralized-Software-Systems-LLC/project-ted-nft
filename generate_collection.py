a6f5b1d98594fa3ac1eeab88a687c1164271edbc
bighappyface <anomalous20@gmail.com>
2021-10-27 20:30:13 -0400

    Add metadata and threading to generate_collection.py

--- generate_collection.py
+++ generate_collection.py
@@ -1,4 +1,6 @@
 from PIL import Image
+import json
+import concurrent.futures
 
 # Start with digits for number of image sets
 # e.g. 11 = 2 image sets, 111 = 3 image sets
@@ -17,11 +19,15 @@ counter = start_id
 # Array of parts
 parts_groups = ["blue", "green", "red", "teal", "yellow"]
 
-# Loop
-while counter <= max_id:
+# List of combinations
+combos = []
 
+# Max workers
+max_workers=7
+
+def make_ted(combo):
     # Quick breakdown of the ID into parts and color
-    img_id = str(counter)
+    img_id = str(combo)
     bg_color = int(img_id[0]) - 1
     legs_color = int(img_id[1]) - 1
     body_color = int(img_id[2]) - 1
@@ -29,20 +35,58 @@ while counter <= max_id:
     face_color = int(img_id[4]) - 1
 
     # Load respective images
-    background = Image.open("img/parts/" + parts_groups[bg_color] + "/bg.PNG")
+    background = Image.open("img/parts/" + parts_groups[bg_color]  + "/bg.PNG")
     legs = Image.open("img/parts/" + parts_groups[legs_color] + "/legs.PNG")
     body = Image.open("img/parts/" + parts_groups[body_color] + "/body.PNG")
     head = Image.open("img/parts/" + parts_groups[head_color] + "/head.PNG")
     face = Image.open("img/parts/" + parts_groups[face_color] + "/face.PNG")
 
     # Paste images over each other
-    background.paste(legs, (0, 0), legs) 
-    background.paste(body, (0, 0), body) 
-    background.paste(head, (0, 0), head) 
-    background.paste(face, (0, 0), face)
+    background.alpha_composite(legs) 
+    background.alpha_composite(body) 
+    background.alpha_composite(head) 
+    background.alpha_composite(face)
 
     # Output the image
-    background.save("img/collection/" + img_id + ".png", "PNG")
+    background = background.convert('RGB')
+    background.save("img/collection/" + img_id + ".jpg", "JPEG", optimize=True, quality=75)
+
+    # Output the metadata
+    metadata = {
+        "name": "Ted " + img_id,
+        "description": "A Ted from the Project Ted collection",
+        "image": "ipfs://QmQU2ny4seZNoPdTo3aWEfrrqQ7aP2aNEnp83xqFDcoGYZ/" + img_id + ".jpg",
+        "attributes": [
+            {
+                "trait_type": "Background",
+                "value": parts_groups[bg_color]
+            },
+            {
+                "trait_type": "Legs",
+                "value": parts_groups[legs_color]
+            },
+            {
+                "trait_type": "Body",
+                "value": parts_groups[body_color]
+            },
+            {
+                "trait_type": "Head",
+                "value": parts_groups[head_color]
+            },
+            {
+                "trait_type": "Face",
+                "value": parts_groups[face_color]
+            }
+        ]
+    }
+    with open("img/metadata/" + img_id + ".json", "w") as write_file:
+        json.dump(metadata, write_file)
+
+# Loop
+while counter <= max_id:
+
+    # Add combo to the list
+    combos.append(counter)
 
     # Skip 6-0 for ones position
     if counter % 10 == 5:
@@ -63,7 +107,9 @@ while counter <= max_id:
         counter += 5000
 
 
-    
+# Distribute output to the thread pool
+with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        executor.map(make_ted, combos)
 
     
     
